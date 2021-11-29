fbe4c1aea4fe6260737ba3ad36f8cd562e107624
Yonatan Schachter <yonatan.schachter@gmail.com>
2021-11-30 00:40:43 +0200

    drivers: serial: Added support for raspberry pi
    Added a serial driver for the RP2040. Only polling
    API is supported.
    
    Signed-off-by: Yonatan Schachter <yonatan.schachter@gmail.com>

---
+++ drivers/serial/uart_rpi_pico.c
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2021, Yonatan Schachter
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <drivers/uart.h>
+#include <drivers/pinctrl.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+#include <hardware/uart.h>
+#include <hardware/gpio.h>
+#pragma GCC diagnostic pop
+
+struct uart_rpi_config {
+	uart_inst_t *const uart_dev;
+	uint32_t baudrate;
+	const struct pinctrl_dev_config *pcfg;
+};
+
+static int uart_rpi_poll_in(const struct device *dev, unsigned char *c)
+{
+	const struct uart_rpi_config *config = dev->config;
+
+	if (!uart_is_readable(config->uart_dev)) {
+		return -1;
+	}
+
+	*c = (unsigned char)uart_get_hw(config->uart_dev)->dr;
+	return 0;
+}
+
+static void uart_rpi_poll_out(const struct device *dev, unsigned char c)
+{
+	const struct uart_rpi_config *config = dev->config;
+
+	uart_putc_raw(config->uart_dev, c);
+}
+
+static int uart_rpi_init(const struct device *dev)
+{
+	const struct uart_rpi_config *config = dev->config;
+	int ret;
+
+	ret = pinctrl_apply_state(config->pcfg, PINCTRL_STATE_DEFAULT);
+	if (ret < 0) {
+		return ret;
+	}
+
+	return !uart_init(config->uart_dev, config->baudrate);
+}
+
+static const struct uart_driver_api uart_rpi_driver_api = {
+	.poll_in = uart_rpi_poll_in,
+	.poll_out = uart_rpi_poll_out,
+};
+
+#define DT_DRV_COMPAT raspberrypi_pico_uart
+
+#define RPI_UART_INIT(idx)						\
+	PINCTRL_DT_INST_DEFINE(idx);					\
+	static const struct uart_rpi_config uart_rpi_cfg_##idx = {	\
+		.uart_dev = (uart_inst_t *)DT_INST_REG_ADDR(idx),	\
+		.baudrate = DT_INST_PROP(idx, current_speed),		\
+		.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(idx),		\
+	};								\
+									\
+	DEVICE_DT_INST_DEFINE(idx, &uart_rpi_init,			\
+				NULL,					\
+				NULL,					\
+				&uart_rpi_cfg_##idx, PRE_KERNEL_1,	\
+				CONFIG_KERNEL_INIT_PRIORITY_DEVICE,	\
+				&uart_rpi_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(RPI_UART_INIT)
